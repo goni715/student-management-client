e436386db8ffa7aa2e07b79819b2d8017bfb1c1c
goni715 <halesgoni715149123@gmail.com>
2024-12-29 20:57:19 +0600

    admin & parent page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+
+const AdminPage = () => {
+    return (
+        <>
+          <h1>This is Admin Page</h1>   
+        </>
+    );
+};
+
+export default AdminPage;
---
+++ src/app/parent/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ParentPage = () => {
+    return (
+        <>
+            <h1>This is Parent Page</h1>
+        </>
+    );
+};
+
+export default ParentPage;
