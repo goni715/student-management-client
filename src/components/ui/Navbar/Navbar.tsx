553bba1ebfb8a65659b7df4a1a84f86963bc1c66
goni715 <halesgoni715149123@gmail.com>
2024-12-30 13:38:36 +0600

    navbar

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 
 const Navbar = () => {
@@ -9,9 +10,22 @@ const Navbar = () => {
                     <Image src="/search.png" alt="search" width={14} height={14}/>
                     <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"/>
                 </div>
-                {/* Icons & User */}
-                <div>
-                    user
+                {/* Icons & User  */}
+                <div className="flex items-center gap-6 justify-end w-full">
+                    <div className="w-7 h-7 bg-white flex items-center justify-center cursor-pointer rounded-full">
+                      <Image src="/message.png" alt="search" width={20} height={20}/>
+                    </div>
+                    <div className="w-7 h-7 bg-white flex items-center justify-center cursor-pointer rounded-full relative">
+                      <Image src="/announcement.png" alt="announcement" width={20} height={20}/>
+                      <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white text-xs rounded-full">
+                        1
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                        <span className="text-xs leading-3 font-medium">Osman Goni</span>
+                        <span className="text-[10px] text-gray-500 text-right">Admin</span>
+                    </div>
+                    <Image src="/avatar.png" alt="avatar" width={36} height={36} className="rounded-full"/>
                 </div>
             </div>
         </>
