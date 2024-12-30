c1a3e763e216ce8650ea022daddbb054c951a055
goni715 <halesgoni715149123@gmail.com>
2024-12-30 13:21:18 +0600

    search box

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <>
-            <div className="flex items-center justify-between bg-amber-400 p-4">
+            <div className="flex items-center justify-between p-4">
                 {/* Search Bar */}
-                <div className="hidden md:flex items-center">
-                    <Image src="/" alt="search"/>
+                <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+                    <Image src="/search.png" alt="search" width={14} height={14}/>
+                    <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"/>
                 </div>
                 {/* Icons & User */}
                 <div>
