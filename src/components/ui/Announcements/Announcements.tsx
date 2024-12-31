c9aba1ae9082edcc07630d5a22e6f7612869c568
goni715 <halesgoni715149123@gmail.com>
2024-12-31 17:39:40 +0600

    title

--- src/components/ui/Announcements/Announcements.tsx
+++ src/components/ui/Announcements/Announcements.tsx
@@ -2,7 +2,29 @@
 const Announcements = () => {
     return (
         <>
-            <h1>This is Announcements components</h1>
+            <div className="p-4 rounded-md">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl font-semibold"> Announcements</h1>
+                    <span className="text-xs text-gray-400">View All</span>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <h2>Lorem Ipsum Donor set</h2>
+                            <span>
+                                2025-01-01
+                            </span>
+                        </div>
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+                        expedita. Rerum, quidem facilis?
+                        </p>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
