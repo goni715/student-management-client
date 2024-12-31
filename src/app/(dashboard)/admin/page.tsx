3150188bf9e059127e1470ecd1ec3e7eaa5702dc
goni715 <halesgoni715149123@gmail.com>
2024-12-31 12:36:27 +0600

    events added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -9,7 +9,7 @@ const AdminPage = () => {
       <>
         <div className="flex flex-col md:flex-row gap-4 p-4">
           {/* Left */}
-          <div className="w-full lg:w-2/3 flex flex-col gap-8 bg-cyan-500">
+          <div className="w-full lg:w-2/3 flex flex-col gap-8">
             {/* user cards */}
             <div className="flex gap-4 flex-wrap justify-between">
               <UserCard type="student" />
@@ -21,11 +21,11 @@ const AdminPage = () => {
             {/* Middle Chart */}
             <div className="flex flex-col lg:flex-row gap-4">
               {/* Count Chart */}
-              <div className="w-full lg:w-1/3 h-[450px] bg-blue-400">
+              <div className="w-full lg:w-1/3 h-[450px]">
                 <CountChart />
               </div>
               {/* Attendence Chart */}
-              <div className="w-full lg:w-2/3 h-[450px] bg-pink-300">
+              <div className="w-full lg:w-2/3 h-[450px]">
                 <AttendenceChart />
               </div>
             </div>
--- src/components/ui/Calendar/EventCalendar.tsx
+++ src/components/ui/Calendar/EventCalendar.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { events } from '@/data/data';
+import Image from 'next/image';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -12,8 +14,26 @@ const EventCalendar = () => {
 
     return (
         <>
-            <div>
+            <div className="bg-white p-4 rounded-md">
               <Calendar onChange={onChange} value={value} />
+              <div className="flex items-center justify-between">
+                <h1 className="text-xl font-semibold my-4">Events</h1>
+                <Image src="/moreDark.png" alt="more_dark" width={20} height={20} />
+              </div>
+              <div className="flex flex-col gap-4">
+                {events?.map((item, i)=> (
+                    <div 
+                      key={i}
+                      className="p-5 border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple rounded-md"
+                    >
+                        <div className="flex items-center justify-between">
+                           <h1 className="font-semibold text-gray-600"> {item.title}</h1>
+                           <span className="text-gray-300 text-xs">{item.time}</span>
+                        </div>
+                        <p className="mt-2 text-gray-400 text-sm">{item.description}</p>
+                    </div>
+                ))}
+              </div>
             </div>
         </>
     );
