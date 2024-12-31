d49a394852a3c905885304361aca33247ecffe95
goni715 <halesgoni715149123@gmail.com>
2024-12-31 12:19:45 +0600

    events data

--- src/components/ui/Calendar/EventCalendar.tsx
+++ src/components/ui/Calendar/EventCalendar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 type ValuePiece = Date | null;
 
--- src/data/data.ts
+++ src/data/data.ts
@@ -112,3 +112,26 @@ export const menuItems = [
       ],
     },
   ];
+
+
+
+  export const events = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
