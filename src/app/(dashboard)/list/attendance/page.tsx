efd1b6005fffc1004205ef7412efe5505e637b73
goni715 <halesgoni715149123@gmail.com>
2025-01-01 18:54:05 +0600

    attendance & results page

---
+++ src/app/(dashboard)/list/attendance/page.tsx
@@ -0,0 +1,10 @@
+
+const AttendanceListPage = () => {
+    return (
+        <>
+            <h1>This is Attendance List Page</h1>
+        </>
+    );
+};
+
+export default AttendanceListPage;
---
+++ src/app/(dashboard)/list/results/page.tsx
@@ -0,0 +1,10 @@
+
+const ResultListPage = () => {
+    return (
+        <>
+            <h1>This is Result List Page</h1>
+        </>
+    );
+};
+
+export default ResultListPage;
