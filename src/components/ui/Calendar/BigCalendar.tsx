b36a71a89d5866663ac34e23bad107854b79e9dd
goni715 <halesgoni715149123@gmail.com>
2025-01-08 21:19:16 +0600

    big calendar completed

--- src/components/ui/Calendar/BigCalendar.tsx
+++ src/components/ui/Calendar/BigCalendar.tsx
@@ -2,27 +2,32 @@
 import { calendarEvents } from "@/data/data";
 import moment from "moment";
 import { useState } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
 const BigCalendar = () => {
-    // const [view, setView] = useState<View>(Views.WORK_WEEK);
+    const [view, setView] = useState<View>(Views.WORK_WEEK);
 
-    // const handleOnChangeView = (selectedView: View) => {
-    //   setView(selectedView);
-    // };
+    const handleOnChangeView = (selectedView: View) => {
+      setView(selectedView);
+    };
 
     return (
       <>
-   <Calendar
-      localizer={localizer}
-      events={calendarEvents}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
+        <Calendar
+          localizer={localizer}
+          events={calendarEvents}
+          startAccessor="start"
+          endAccessor="end"
+          views={["work_week", "day"]}
+          view={view}
+          onView={handleOnChangeView}
+          style={{ height: '98%' }}
+          min={new Date(2025, 1, 0, 8, 0, 0)}
+          max={new Date(2025, 1, 0, 17, 0, 0)}
+        />
       </>
     );
 };
