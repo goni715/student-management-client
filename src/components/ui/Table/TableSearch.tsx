2c0944fa14c0da3d812da8b9eb7ca24a459bd7e4
goni715 <halesgoni715149123@gmail.com>
2025-01-12 12:43:48 +0600

    table serach

--- src/components/ui/Table/TableSearch.tsx
+++ src/components/ui/Table/TableSearch.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 
 const TableSearch = () => {
     return (
         <>
-            This is Table Search Component
+            <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+                <Image src="/search.png" alt="table_search" width={14} height={14}/>
+                <input
+                  type="text" 
+                  placeholder="Search..."
+                  className="w-[200px] p-2 bg-transparent outline-none"
+                />
+            </div>
         </>
     );
 };
