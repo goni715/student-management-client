f2803f0e3f2d938056400245123a0b7c572ab3e8
goni715 <halesgoni715149123@gmail.com>
2025-01-22 18:15:24 +0600

    pagination

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -2,8 +2,25 @@
 const Pagination = () => {
     return (
         <>
-           <div>
-              <h1>This is Pagination Component</h1>
+           <div className="flex p-4 items-center justify-between text-gray-500">
+              <button
+                disabled
+                className="py-2 px-4 bg-slate-200 text-xs font-semibold rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Prev
+              </button>
+              <div className="flex items-center gap-2 text-sm">
+                <button className="px-2 rounded-sm bg-lamaSky">1</button>
+                <button className="px-2 rounded-sm">2</button>
+                <button className="px-2 rounded-sm">3</button>
+                ...
+                <button className="px-2 rounded-sm">4</button>
+              </div>
+              <button
+                className="py-2 px-4 bg-slate-200 text-xs font-semibold rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Next
+              </button>
            </div> 
         </>
     );
