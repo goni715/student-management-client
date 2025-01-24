49b8de23a886d3196d08545e4d6f8153d6f4123a
goni715 <halesgoni715149123@gmail.com>
2025-01-24 12:45:18 +0600

    Form Modal

--- src/components/ui/Modal/FormModal.tsx
+++ src/components/ui/Modal/FormModal.tsx
@@ -1,15 +1,41 @@
+import Image from "next/image";
 
 type TProps = {
-    table: string;
-    type: string;
-}
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+    type: "create" | "update" | "delete";
+  data?: any;
+  id?: number;
+};
 
-const FormModal = ({table, type}: TProps) => {
-    return (
-        <>
-            This is Form Modal Component {table} ${type}
-        </>
-    );
+const FormModal = ({ table, type, data, id }: TProps) => {
+
+    const bgColor =
+    type === "create"
+      ? "bg-lamaYellow"
+      : type === "update"
+      ? "bg-lamaSky"
+      : "bg-lamaPurple";
+
+
+  return (
+    <>
+       <button className={`flex items-center justify-center rounded-full ${bgColor}`}>
+         <Image src={`/${type}.png`} alt="" width={16} height={16}/>
+       </button>
+    </>
+  );
 };
 
 export default FormModal;
