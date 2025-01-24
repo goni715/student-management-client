25e0e96fc602c90a5289da0a19feb6b346eafcbf
goni715 <halesgoni715149123@gmail.com>
2025-01-24 11:57:55 +0600

    student type

---
+++ src/types/student.type.ts
@@ -0,0 +1,11 @@
+export type TStudent = {
+    id: number;
+    studentId: string;
+    name: string;
+    email?: string;
+    photo: string;
+    phone?: string;
+    grade: number;
+    class: string;
+    address: string;
+  };
