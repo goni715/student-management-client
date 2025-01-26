d88a0b0f1c27bf7ae8386eb470dbd3f8645184a6
goni715 <halesgoni715149123@gmail.com>
2025-01-26 23:08:08 +0600

    parents-data

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,9 +1,63 @@
+import Pagination from "@/components/ui/Pagination/Pagination";
+import OGTable from "@/components/ui/Table/OGTable";
+import TableSearch from "@/components/ui/Table/TableSearch";
+import { parentsData } from "@/data/parent.data";
+import Image from "next/image";
 
+const columns = [
+    {
+      header: "Info",
+      accessor: "info",
+    },
+    {
+      header: "Student Names",
+      accessor: "students",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Phone",
+      accessor: "phone",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Actions",
+      accessor: "action",
+    },
+  ];
+
+  
 const ParentListPage = () => {
     return (
-        <>
-           <h1>This is Parent List Page</h1> 
-        </>
+      <>
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+          {/* TOP */}
+          <div className="flex items-center justify-between">
+            <h1 className="hidden md:block text-lg font-semibold">
+              All Parents
+            </h1>
+            <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+              <TableSearch />
+              <div className="flex items-center gap-4 self-end">
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                  <Image src="/filter.png" alt="" width={14} height={14} />
+                </button>
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                  <Image src="/sort.png" alt="" width={14} height={14} />
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* LIST */}
+          <OGTable columns={columns} renderRow={renderRow} data={parentsData} />
+          {/* PAGINATION */}
+          <Pagination />
+        </div>
+      </>
     );
 };
 
