77d9de137e51d547380ac55003db06399c8f7f97
goni715 <halesgoni715149123@gmail.com>
2025-01-26 23:10:42 +0600

    parent list

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 import Pagination from "@/components/ui/Pagination/Pagination";
 import OGTable from "@/components/ui/Table/OGTable";
 import TableSearch from "@/components/ui/Table/TableSearch";
 import { parentsData } from "@/data/parent.data";
+import { TParent } from "@/types/parent.type";
 import Image from "next/image";
 
 const columns = [
@@ -32,6 +34,32 @@ const columns = [
 
   
 const ParentListPage = () => {
+    const renderRow = (item: TParent) => (
+        <tr
+          key={item.id}
+          className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+        >
+          <td className="flex items-center gap-4 p-4">
+            <div className="flex flex-col">
+              <h3 className="font-semibold">{item.name}</h3>
+              <p className="text-xs text-gray-500">{item?.email}</p>
+            </div>
+          </td>
+          <td className="hidden md:table-cell">{item.students.join(",")}</td>
+          <td className="hidden md:table-cell">{item.phone}</td>
+          <td className="hidden md:table-cell">{item.address}</td>
+          <td>
+            <div className="flex items-center gap-2">
+              {/* {role === "admin" && (
+                <>
+                  <FormModal table="parent" type="update" data={item} />
+                  <FormModal table="parent" type="delete" id={item.id} />
+                </>
+              )} */}
+            </div>
+          </td>
+        </tr>
+      );
     return (
       <>
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
